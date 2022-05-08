1546d6c6bb773d2a0bc7f3d4db8bbec2606936b5
Hussein Omar <husseinomar6190@gmail.com>
2022-05-09 02:32:06 +0300

    installing psycopg2 and creatinf a postgress database

--- pitches/__init__.py
+++ pitches/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628b453b0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mwas6190@localhost/pitches'
 
 db = SQLAlchemy(app)
 from pitches import views
--- pitches/models.py
+++ pitches/models.py
@@ -19,9 +19,9 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    comments = db.relationship('Comments', backref='topic', lazy=True)
+    comments = db.relationship('Comments', backref='topicpost', lazy=True)
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+        return f"Post('{self.topic}', '{self.date_posted}')"
        
        
 class Comments(db.Model):
