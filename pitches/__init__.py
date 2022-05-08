665283ebfdfcda82bb8b8ac6161a901c989930c3
Hussein Omar <husseinomar6190@gmail.com>
2022-05-09 01:15:20 +0300

    creating models for the database

--- pitches/__init__.py
+++ pitches/__init__.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628b453b0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
+db = SQLAlchemy(app)
 from pitches import views
