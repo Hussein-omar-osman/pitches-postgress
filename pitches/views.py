27be2118c8a12b624febb693f398233b3935bd33
Hussein Omar <husseinomar6190@gmail.com>
2022-05-08 21:33:25 +0300

    creating navlinks templates

--- pitches/views.py
+++ pitches/views.py
@@ -4,3 +4,15 @@ from pitches import app
 @app.route('/')
 def home():
  return render_template('home.html', title='Home')
+
+@app.route("/about")
+def about():
+ return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+ return render_template('register.html', title='Register')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+ return render_template('login.html', title='Login')
