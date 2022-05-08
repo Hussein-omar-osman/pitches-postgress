7afa68d03a0cc4e7903ff932db6b620753d21cf5
Hussein Omar <husseinomar6190@gmail.com>
2022-05-09 00:16:32 +0300

    installing flask sqlalchemy and creating an instance

--- pitches/__init__.py
+++ pitches/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628b453b0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
 from pitches import views
