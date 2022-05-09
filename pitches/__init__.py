91107065759a499e03371be162d5b19ef8e0eb5c
Hussein Omar <husseinomar6190@gmail.com>
2022-05-09 12:26:08 +0300

    adding flask bcrypt hashing module

--- pitches/__init__.py
+++ pitches/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ app.config['SECRET_KEY'] = '5791628b453b0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mwas6190@localhost/pitches'
 
 db = SQLAlchemy(app)
+bc = Bcrypt(app)
 from pitches import views
