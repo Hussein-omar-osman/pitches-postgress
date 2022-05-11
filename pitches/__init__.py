f991217701863e13bf7597f958103208b6d5aa08
Hussein Omar <husseinomar6190@gmail.com>
2022-05-11 15:36:52 +0300

    deploying to production

--- pitches/__init__.py
+++ pitches/__init__.py
@@ -9,12 +9,13 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'this-is-my-very-secret-key'
 
-env = 'dev'
+env = 'prod'
 if env == 'dev':
  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mwas6190@localhost/pitches'
  app.debug = True
 else:
  app.debug = False
+ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nistfehdirgxrx:ae412f0a7de50e43fd1096d7a0cf3967d9585beed5aa7c5d7cb96ce76614de2e@ec2-52-5-110-35.compute-1.amazonaws.com:5432/daglk5fjt7k082'
 
 db = SQLAlchemy(app)
 bc = Bcrypt(app)
